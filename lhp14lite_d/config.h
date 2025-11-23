60fa5b326a3520e18845eef03ca780855c7783d8
タマネギ５１ <Asz7yu3we@outlook.jp>
2025-11-23 21:22:02 +0900

    Update config.h

--- lhp14lite_d/config.h
+++ lhp14lite_d/config.h
@@ -1,5 +1,13 @@
 #pragma once
 
+// ジョイスティックの軸数を定義
+#define JOYSTICK_AXES 2   // X軸とY軸
+
+// ADCピンの定義（例: ProMicroのF4/F5ピン）
+#define JOYSTICK_AXIS_X_PIN F4
+#define JOYSTICK_AXIS_Y_PIN F5
+
+
 #include "config_user.h"
 
 /* USB設定 */
