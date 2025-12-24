d84c1fc62cad22a2bec32c3e720f09bc6c9efbe2
タマネギ５１ <Asz7yu3we@outlook.jp>
2025-12-24 19:15:17 +0900

    Update keymap.c

--- lhp14lite_d/keymaps/default/keymap.c
+++ lhp14lite_d/keymaps/default/keymap.c
@@ -1,3 +1,6 @@
+#include "analog.h"
+#include "pointing_device.h"
+
 #include QMK_KEYBOARD_H
 #include "analog.h"
 #include "pointing_device.h"
@@ -53,4 +56,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q, KC_R, KC_S, KC_T
     )
 };
+static void read_joystick(void) {
+    uint16_t raw_x = analogReadPin(JOY_X_PIN);
+    uint16_t raw_y = analogReadPin(JOY_Y_PIN);
+    bool     sw    = (readPin(JOY_SW_PIN) == 0);
+
+    int16_t dx = (int16_t)raw_x - JOY_CENTER;
+    int16_t dy = (int16_t)raw_y - JOY_CENTER;
+
+    if (dx > -JOY_DEADZONE && dx < JOY_DEADZONE) dx = 0;
+    if (dy > -JOY_DEADZONE && dy < JOY_DEADZONE) dy = 0;
+
+    dx /= 32;
+    dy /= 32;
+
+    joy_x = dx;
+    joy_y = -dy;
+    joy_sw = sw;
+}
+
+report_mouse_t pointing_device_task(report_mouse_t mouse_report) {
+    read_joystick();
+
+    mouse_report.x = joy_x;
+    mouse_report.y = joy_y;
+
+    if (joy_sw) {
+        mouse_report.buttons |= MOUSE_BTN1;
+    } else {
+        mouse_report.buttons &= ~MOUSE_BTN1;
+    }
+
+    return mouse_report;
+}
 
